b4600d49795885c136e37ea8c15834ab84abeb25
Damian Lance <damian.lance@gmail.com>
2020-06-03 08:58:13 -0700

    Update utils.py

--- randomarchive/users/utils.py
+++ randomarchive/users/utils.py
@@ -38,6 +38,4 @@ def upload_blob(source_file_name, destination_blob_name):
         )
     )
     
-    picture_fn = 'https://storage.cloud.google.com/' + bucket_name + '/' + destination_blob_name + '?cloudshell=true&orgonly=true&supportedpurview=organizationId'    
-    
-    return picture_fn
+    return destination_blob_name
