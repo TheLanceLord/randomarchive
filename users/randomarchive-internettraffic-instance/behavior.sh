f49c5cdf6c60cd657db3ea848e754ec6aa858527
Damian Lance <damian.lance@gmail.com>
2020-06-25 15:31:42 -0700

    Update behavior.sh

--- users/randomarchive-internettraffic-instance/behavior.sh
+++ users/randomarchive-internettraffic-instance/behavior.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true
 do        
-        gcloud compute forwarding-rules list --filter="NAME=randomarchive-webapp-fe">loadbalancer.txt
+        gcloud compute forwarding-rules list --filter="NAME ~ randomarchive">loadbalancer.txt
         RANDOMARCHIVELB_IP=$(awk 'NR == 2 {print $3}' loadbalancer.txt)
         #echo "Current randomarchive loadbalancer ip is: $RANDOMARCHIVELB_IP"
         for i in $(seq 1 1800)
