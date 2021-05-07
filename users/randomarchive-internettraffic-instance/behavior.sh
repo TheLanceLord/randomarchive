c06774895051c7dfdea5ef8a78ea348457fb9877
Damian Lance <damian.lance@gmail.com>
2021-05-07 08:37:13 -0700

    Update behavior.sh

--- users/randomarchive-internettraffic-instance/behavior.sh
+++ users/randomarchive-internettraffic-instance/behavior.sh
@@ -4,7 +4,7 @@ while true
 do        
         RANDOMARCHIVELB_IP=$(gcloud compute forwarding-rules list --filter="NAME ~ randomarchive" --format=json | jq -r '.[] | .IPAddress')
         #echo "Current randomarchive loadbalancer ip is: $RANDOMARCHIVELB_IP"
-        for i in $(seq 1 1800)
+        for i in $(seq 1 60)
         do
                 curl -I http://"$RANDOMARCHIVELB_IP"
                 sleep 1
