6230233698901eeedb19bdf7e303068a738ca8da
Damian Lance <damian.lance@gmail.com>
2021-07-01 09:05:41 -0700

    Update routes.py

--- randomarchive/users/routes.py
+++ randomarchive/users/routes.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
 from flask import render_template, url_for, flash, redirect, request, Blueprint, Flask, session, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from randomarchive import db, bcrypt, mysql
