32a89c3fd06250e6603c59f4c1c0990cb5d344c3
Damian Lance <damian.lance@gmail.com>
2021-07-01 08:17:27 -0700

    Update behavior.sh

--- users/randomarchive-kendall-instance/behavior.sh
+++ users/randomarchive-kendall-instance/behavior.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#####################################################################################################################
+# IMPORTANT - BEFORE YOU PROCEED!!!!                                                                                
+# If you are doing this breakage lab as part of an instructor-led training, we ask that you don't look through the 
+# user behavior code, as in a real world scenario you wouldn't have a convenient script readily available that
+# tells you what a user is doing. If you have questions about a users behavior feel free to ask your instructor!
+# If you are doing this breakage lab as a self-paced training on Qwiklabs and if no user context is provided, then
+# proceed if you feel stuck, or if you would feel that talking with this user would be the next rational step
+#####################################################################################################################
 sudo apt-get install -y jq
 while true
 do
